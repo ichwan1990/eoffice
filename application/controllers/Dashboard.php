cb5033f5cb93c357da39ad956341899f2ba3441b
Ichwan.ElHamdhani <boootx@gmail.com>
2024-03-14 15:34:13 +0700

    cek dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -18,7 +18,7 @@ class Dashboard extends CI_Controller
 
 	public function index()
 	{
-		$this->output->enable_profiler(TRUE);
+		//$this->output->enable_profiler(TRUE);
 		$data = array(
 			'surat_masuk' => $this->surat_in->get()->num_rows(),
 			'surat_keluar' => 0, //$this->surat_out->get()->num_rows(),
